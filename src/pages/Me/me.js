cbcea3fccef8dafd9f08f5e7efad9f4d4293e997
yuslianto <yuslianto56@gmail.com>
2019-12-26 17:18:08 +0700

    make styled global and make abaout me

--- src/pages/Me/me.js
+++ src/pages/Me/me.js
@@ -1,11 +1,32 @@
 import React from 'react';
 
+import { SectionTitle, Paragraph, Pill } from '../../styles';
 import { ProfileLink } from './styles';
 
 const Me = ({ user }) => {
   return (
     <div>
-        this is me
+
+      <div>
+        <SectionTitle>About Me</SectionTitle>
+        <Paragraph>user.basic.summary</Paragraph>
+      </div>
+
+      <div>
+        <div>
+          <SectionTitle>Skils</SectionTitle>
+          <div>
+            {/*
+              mapping skills item
+            */}
+            <Pill>name skill</Pill>
+            <Pill>name skill</Pill>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <SectionTitle>Profiles</SectionTitle>
         <ul>
           <ProfileLink>
             <a href="/">
@@ -13,6 +34,8 @@ const Me = ({ user }) => {
             </a>
           </ProfileLink>
         </ul>
+      </div>
+
     </div>
   );
 };
